5f51c112a02f501792f8dc2fdbb323d5e31c0a00
code4Shawn <54803973+code4Shawn@users.noreply.github.com>
2020-04-21 18:54:21 +0300

    refinement

--- Lab/Crud.php
+++ Lab/Crud.php
@@ -1,13 +1,11 @@
-<?php 
-interface Crud{
-	public function save();
-	public function readAll();
-	public function readUnique();
-	public function search();
-	public function update();
-	public function removeOne();
-	public function removeAll();
-}
-
-
- ?>
+<?php
+	interface Crud{
+		public function save();
+		public function readAll();
+		public function readUnique();
+		public function search();
+		public function update();
+		public function removeOne();
+		public function removeAll();
+	}
+?>
